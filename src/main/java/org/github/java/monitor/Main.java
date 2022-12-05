d45e2fc84da2500c779a569d1dfa266420b01018
liulijun <neteasellj@qq.com>
2022-12-05 12:31:20 +0800

    feat: init project

---
+++ src/main/java/org/github/java/monitor/Main.java
@@ -0,0 +1,13 @@
+package org.github.java.monitor;
+
+/**
+ * description
+ *
+ * @author LiJun.Liu
+ * @since $DATE
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
